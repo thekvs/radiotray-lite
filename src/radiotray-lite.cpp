8d973e5a33168d6ab18591b651462652a417d724
Konstantin Sorokin <kvs@sigterm.ru>
2017-01-27 13:17:40 +0300

    less verbose logging in Release mode build
    + do not create logfile in file system

--- src/radiotray-lite.cpp
+++ src/radiotray-lite.cpp
@@ -7,13 +7,24 @@ using namespace radiotray;
 int
 main(int argc, char* argv[])
 {
-    el::Configurations defaultConf;
-    defaultConf.setToDefault();
+    el::Configurations easylogging_config;
+    easylogging_config.setToDefault();
     // Values are always std::string
-    defaultConf.set(el::Level::Info, el::ConfigurationType::Format, "%datetime %level %loc %msg");
-    defaultConf.set(el::Level::Error, el::ConfigurationType::Format, "%datetime %level %loc %msg");
+
+#ifndef NDEBUG
+    easylogging_config.set(el::Level::Info, el::ConfigurationType::Format, "%datetime %level %loc %msg");
+    easylogging_config.set(el::Level::Error, el::ConfigurationType::Format, "%datetime %level %loc %msg");
+    easylogging_config.set(el::Level::Warning, el::ConfigurationType::Format, "%datetime %level %loc %msg");
+#else
+    easylogging_config.set(el::Level::Info, el::ConfigurationType::Format, "%level %msg");
+    easylogging_config.set(el::Level::Error, el::ConfigurationType::Format, "%level %msg");
+    easylogging_config.set(el::Level::Warning, el::ConfigurationType::Format, "%level %msg");
+#endif
+    // do not log to a file
+    easylogging_config.setGlobally(el::ConfigurationType::ToFile, "false");
+
     // default logger uses default configurations
-    el::Loggers::reconfigureLogger("default", defaultConf);
+    el::Loggers::reconfigureLogger("default", easylogging_config);
 
     RadioTrayLite rtl;
 
